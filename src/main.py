8beca8f1db3dcec58ed42e296c85a8b21db08947
Damon German <damon.german@hyland.com>
2024-11-21 15:56:37 -0600

    Rename and refactor `repository.py` to `student_repository.py`, adding `StudentRepository` class with average grade point calculation.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 ﻿from core.status import Status
 from core.student import Student
 from core.student_data_service import StudentDataService
+from src.core.student_repository import StudentRepository
 
 
 def main():
@@ -14,6 +15,9 @@ def main():
 
     student = Student(id=1, first_name="John", last_name="Doe", grade_point=4, status=Status.ACTIVE)
     print(student.__dict__)
+
+    student_repository = StudentRepository(service)
+    print(student_repository.average_grade_point())
 
 
 if __name__ == '__main__':
