7ef250e42b33dbd0917a72e13bc8dbbc092ac8bb
Damon German <damon.german@hyland.com>
2024-11-21 16:10:21 -0600

    Refactored main function to streamline StudentDataService initialization and simplify code.

--- src/main.py
+++ src/main.py
@@ -5,19 +5,13 @@ from src.core.student_repository import StudentRepository
 
 
 def main():
-    students = [
+    service = StudentDataService([
         Student(id=1, first_name="John", last_name="Doe", grade_point=4, status=Status.ACTIVE),
         Student(id=2, first_name="Jane", last_name="Doe", grade_point=3, status=Status.INACTIVE),
-    ]
-
-    service = StudentDataService(students)
+    ])
     print(service.get_students())
 
-    student = Student(id=1, first_name="John", last_name="Doe", grade_point=4, status=Status.ACTIVE)
-    print(student.__dict__)
-
-    student_repository = StudentRepository(service)
-    print(student_repository.average_grade_point())
+    print(StudentRepository(service).average_grade_point())
 
 
 if __name__ == '__main__':
