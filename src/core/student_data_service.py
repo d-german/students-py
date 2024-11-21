239f9a960193943cf844b9acc9ef1971d4344628
Damon German <damon.german@hyland.com>
2024-11-21 14:41:28 -0600

    Add StudentDataService class for managing student data and update main to utilize it.

---
+++ src/core/student_data_service.py
@@ -0,0 +1,11 @@
+﻿from typing import List
+
+from src.core.student import Student
+
+
+class StudentDataService:
+    def __init__(self, students: List['Student']):  # Specify that students is a list of Student objects
+        self._students = students
+
+    def get_students(self) -> List['Student']:  # The method returns a list of Student objects
+        return self._students
--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 ﻿from core.status import Status
 from core.student import Student
+from core.student_data_service import StudentDataService
 
 
 def main():
-    student = Student(
-        id=1,
-        first_name="John",
-        last_name="Doe",
-        grade_point=3.5,
-        status=Status.GRADUATED
-    )
+    students = [
+        Student(id=1, first_name="John", last_name="Doe", grade_point=4, status=Status.ACTIVE),
+        Student(id=2, first_name="Jane", last_name="Doe", grade_point=3, status=Status.INACTIVE),
+    ]
 
-    print(student)
+    service = StudentDataService(students)
+    print(service.get_students())
 
 
 if __name__ == '__main__':
